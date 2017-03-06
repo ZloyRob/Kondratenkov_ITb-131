77649788b9618ab4e76044d1d4d28ef2c29d548e
ZloyRob <vadimandsanek@yandex.ru>
2017-03-06 17:00:20 +0700

    Вернуть exit code

--- src/MainPr.java
+++ src/MainPr.java
@@ -3,6 +3,7 @@
  */
 public class MainPr {
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        System.out.println("Hello World");
+        System.exit(2);
     }
 }
