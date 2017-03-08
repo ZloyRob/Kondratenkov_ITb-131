5405bc6ae8e9b6703ecac349170dd457d185faf7
ZloyRob <vadimandsanek@yandex.ru>
2017-03-09 04:47:41 +0700

    Вывод справки

--- src/Validator.java
+++ src/Validator.java
@@ -8,15 +8,17 @@ import java.util.ArrayList;
 public class Validator {
     AAAService aserv = new AAAService();
     CommandLineParser parser = new DefaultParser();
+
     Options opt = new Options();
     public UserInput Allocation(UserInput us, String[] args){
         opt.addOption("l", "login", true, "Login" );
-        opt.addOption("l", "pass", true, "Password" );
-        opt.addOption("l", "res", true, "Resource" );
-        opt.addOption("l", "role", true, "Role" );
-        opt.addOption("l", "ds", true, "Date start" );
-        opt.addOption("l", "de", true, "Date finish" );
-        opt.addOption("l", "vol", true, "Volume" );
+        opt.addOption("p", "pass", true, "Password" );
+        opt.addOption("re", "res", false, "Resource" );
+        opt.addOption("ro", "role", false, "Role" );
+        opt.addOption("ds", "ds", false, "Date start" );
+        opt.addOption("de", "de", false, "Date finish" );
+        opt.addOption("v", "vol", false, "Volume" );
+        opt.addOption("h", "help", false, "Help" );
         try{
         CommandLine line = parser.parse( opt, args );
             us.login=line.getOptionValue("login");
@@ -26,9 +28,16 @@ public class Validator {
             us.dss=line.getOptionValue("ds");
             us.des=line.getOptionValue("de");
             us.vols=line.getOptionValue("vol");
+            if(line.hasOption("h")|| null==us.login || null==us.pass ){
+                HelpFormatter fo = new HelpFormatter();
+                fo.printHelp("Help", opt);
+                System.exit(0);
+            }
         }
         catch ( ParseException exp){
-            System.out.println( "Unexpected exception:" + exp.getMessage() );
+            HelpFormatter fo = new HelpFormatter();
+            fo.printHelp("Help", opt);
+            System.exit(0);
         }
         return  us;
     }
