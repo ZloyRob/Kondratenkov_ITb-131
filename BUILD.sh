44c8390fe5e2802572f400252ee6496024165277
ZloyRob <vadimandsanek@yandex.ru>
2017-03-17 02:29:29 +0700

    малые изменения билда

--- BUILD.sh
+++ BUILD.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-javac -cp ".\libs\commons-cli-1.3.1.jar;.\libs\commons-codec-1.10.jar;.\libs\commons-lang3-3.5.jar;." -d "./out/production/Kondratenkov_Vadim" src/*.java #
+javac -cp ".\libs\*" -d "./out/production/Kondratenkov_Vadim" src/*.java #
 echo "Hello World"
 read -p "Press any key..."
