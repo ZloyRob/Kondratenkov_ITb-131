78088e276b203103213c5b7e346addd34c41e40e
ZloyRob <vadimandsanek@yandex.ru>
2017-03-18 15:43:47 +0700

    Update BUILD.sh

--- BUILD.sh
+++ BUILD.sh
@@ -6,7 +6,7 @@ touch manifest.txt
 echo "Manifest-Version: 1.0 " > manifest.txt 
 echo "Created-By: Vadim " >> manifest.txt 
 echo "Main-Class: Main " >> manifest.txt 
-echo "Class-Path: .\libs\commons-lang3-3.5.jar .\libs\commons-cli-1.3.1.jar .\libs\commons-codec-1.10.jar " >> manifest.txt  
+echo "Class-Path: libs/commons-cli-1.3.1.jar libs/commons-codec-1.10.jar libs/commons-lang3-3.5.jar " >> manifest.txt  
 jar cvmf manifest.txt Kondratenkov_Vadim.jar -C ./bin / ./libs/* #
 rm manifest.txt 
 echo "Jar good"
