1f7d5a2af5f9ecb782beeeebcf447ae6b7f976c2
ZloyRob <vadimandsanek@yandex.ru>
2017-04-03 18:17:23 +0700

    UpBUILD

--- BUILD.sh
+++ BUILD.sh
@@ -7,7 +7,7 @@ echo "Manifest-Version: 1.0 " > manifest.txt
 echo "Created-By: Vadim " >> manifest.txt 
 echo "Main-Class: Main " >> manifest.txt 
 echo "Class-Path: libs/commons-cli-1.3.1.jar libs/commons-codec-1.10.jar libs/commons-lang3-3.5.jar libs/flyway-core-4.1.2.jar libs/h2-1.4.194.jar" >> manifest.txt  
-jar cvmf manifest.txt Kondratenkov_Vadim.jar -C ./bin / ./libs/* #
+jar cvmf manifest.txt Kondratenkov_Vadim.jar -C ./bin / ./libs/* ./res #
 rm manifest.txt 
 echo "Jar good"
 rm -rf bin #
