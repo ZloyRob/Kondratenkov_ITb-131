3fcfe86100d660b8ad658535441ebf093a02869f
ZloyRob <vadimandsanek@yandex.ru>
2017-04-16 01:44:10 +0700

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import org.apache.logging.log4j.Logger;
 import org.flywaydb.core.Flyway;
 
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 
@@ -19,46 +20,20 @@ public class Main {
         } catch (Exception e) {
             flyway.baseline();
         }
-       /* ArrayList<User> users = new ArrayList<>(); //Создаем коллекцию пользователй
 
-        ArrayList<Resource> resources = new ArrayList<>(); //Создаем коллекцию ресурсов
-
-
-        ArrayList<Accounting> journal = new ArrayList<>(); //список всех сессий*/
         UserInput userInput = new UserInput();
         Validator valid = new Validator();
         valid.allocation(userInput, args);
-       /* try {
-            dbConnection = getDBConnection();
-            Statement statement = dbConnection.createStatement();
-            ResultSet result = statement.executeQuery("SELECT * FROM USER");
-            while (result.next()) {
-                users.add(new User(result.getString("LOGIN"), result.getString("PASS"), Integer.valueOf(result.getString("ID")),result.getString("SALT")));
-                //System.out.println(result.getString("ID") + " " + result.getString("LOGIN") + " " + result.getString("PASS") + " " + result.getString("SALT"));
-            }
-            result = statement.executeQuery("SELECT * FROM RESOURCE");
-            while (result.next()) {
-                resources.add(new Resource(result.getString("PATH"), Integer.valueOf(result.getString("USERID")),result.getString("ROLE"), Integer.valueOf(result.getString("ID"))));
-                //System.out.println(result.getString("USERID") + " " + result.getString("PATH") + " " + result.getString("ROLE"));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
 
-                dbConnection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }*/
         try {
             dbConnection = getDBConnection();
             Statement statement = dbConnection.createStatement();
             ResultSet result = statement.executeQuery( String.format("SELECT * FROM USER WHERE (USER.LOGIN LIKE '%s')", userInput.login));
+            result.next();
             User usbd = new User(result.getString("LOGIN"), result.getString("PASS"), Integer.valueOf(result.getString("ID")),result.getString("SALT"));
             boolean isAuthentication = valid.isAuthentication(usbd, userInput);
             String[] masOfPath = userInput.path.split("\\."); //разбиваем путь по уровням
-            result = statement.executeQuery(String.format("SELECT * FROM RESOURCE where path like '%s%'", masOfPath[0]));
+            result = statement.executeQuery(String.format("SELECT * FROM RESOURCE where path like '%s", masOfPath[0])+"%'");
             ArrayList<Resource> resources = new ArrayList<>();
             while (result.next()) {
                 resources.add(new Resource(result.getString("PATH"), Integer.valueOf(result.getString("USERID")),result.getString("ROLE"), Integer.valueOf(result.getString("ID"))));
@@ -66,8 +41,8 @@ public class Main {
             boolean isAuthorization = valid.isAuthorization(resources, userInput, isAuthentication);
             Accounting journal = new Accounting();
             valid.isAccouting(journal, userInput, isAuthorization);
-
-
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            statement.execute(String.format("Insert into ACCOUNTING (DS, DE , VOL, RESID ) VALUES (PARSEDATETIME('%s','yyyy-MM-dd'), PARSEDATETIME('%s','yyyy-MM-dd'),'%s','%s')", sdf.format(journal.ds), sdf.format(journal.de), journal.vol, journal.resId));
         } catch (Exception e) {
             e.printStackTrace();
         }
