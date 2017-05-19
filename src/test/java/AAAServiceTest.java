d6a8544667012e3b53b074cae8137c6c17867824
ZloyRob <vadimandsanek@yandex.ru>
2017-05-19 23:09:22 +0700

    -Переменные

--- src/test/java/AAAServiceTest.java
+++ src/test/java/AAAServiceTest.java
@@ -9,8 +9,6 @@ import static org.junit.Assert.assertEquals;
 public class AAAServiceTest {
 
     private AAAService aaaService;
-    private UserInput userInput;
-    private User jdoe;
 
     @Before
     public void def() {
@@ -19,59 +17,59 @@ public class AAAServiceTest {
 
     @Test
     public void testCheckPass() {
-        userInput = new UserInput("jdoe","sup3rpaZZ");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ");
         userInput.userId = 1;
-        jdoe = new User("0b6ae56859971bf3a34056e08b293b02", 1, "M5(ITYK");
+        User jdoe = new User("0b6ae56859971bf3a34056e08b293b02", 1, "M5(ITYK");
         assertEquals(true, aaaService.isCheckPass(jdoe, userInput));
     }
 
     @Test
     public void testCheckWrongPass() {
-        userInput = new UserInput("jdoe", "XXX");
+        UserInput userInput = new UserInput("jdoe", "XXX");
         userInput.userId = 1;
-        jdoe = new User("0b6ae56859971bf3a34056e08b293b02", 1, "M5(ITYK");
+        User jdoe = new User("0b6ae56859971bf3a34056e08b293b02", 1, "M5(ITYK");
         assertEquals(false, aaaService.isCheckPass(jdoe, userInput));
     }
 
     @Test
     public void testCheckRole() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a");
         assertEquals(true, aaaService.isCheckRole(userInput));
     }
 
     @Test
     public void testCheckWrongRole() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","XXX","a");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","XXX","a");
         assertEquals(false, aaaService.isCheckRole(userInput));
     }
 
     @Test
     public void testCheckDate() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "100");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "100");
         assertEquals(true, aaaService.isCheckDate(userInput));
     }
 
     @Test
     public void testCheckWrongDate() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "01-01-2015", "2015-12-31", "100");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "01-01-2015", "2015-12-31", "100");
         assertEquals(false, aaaService.isCheckDate(userInput));
     }
 
     @Test
     public void testCheckVol() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "100");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "100");
         assertEquals(true, aaaService.isCheckVol(userInput));
     }
 
     @Test
     public void testCheckWrongVol() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "-100");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "-100");
         assertEquals(false, aaaService.isCheckVol(userInput));
     }
 
     @Test
     public void testCheckStringVol() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "Test");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "Test");
         assertEquals(false, aaaService.isCheckVol(userInput));
     }
 
--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -10,9 +10,6 @@ import static org.junit.Assert.assertEquals;
 public class ValidatorTest {
 
     private Validator validator;
-    private UserInput userInput;
-    private User jdoe;
-    private Resource res;
 
     @Before
     public void def() {
@@ -23,13 +20,13 @@ public class ValidatorTest {
 
     @Test
     public void testEmptyUser() {
-        userInput = new UserInput();
+        UserInput userInput = new UserInput();
         assertEquals(true, userInput.isEmpty());
     }
 
     @Test
     public void testNotEmptyUser() {
-        userInput = new UserInput("TEST","Pass");
+        UserInput userInput = new UserInput("TEST","Pass");
         assertEquals(false, userInput.isEmpty());
     }
 
@@ -47,85 +44,85 @@ public class ValidatorTest {
 
     @Test
     public void testNotLog() {
-        userInput = new UserInput("XXX","XXX");
+        UserInput userInput = new UserInput("XXX","XXX");
         assertEquals(1, validator.isAuthenticationTest(null, userInput));
 
     }
 
     @Test
     public void testNotPass() {
-        userInput = new UserInput("jdoe", "XXX");
-        jdoe = new User("0b6ae56859971bf3a34056e08b293b02", 1, "M5(ITYK");
+        UserInput userInput = new UserInput("jdoe", "XXX");
+        User jdoe = new User("0b6ae56859971bf3a34056e08b293b02", 1, "M5(ITYK");
         assertEquals(2, validator.isAuthenticationTest(jdoe, userInput));
     }
 
     @Test
     public void testLogAndPass() {
-        userInput = new UserInput("jdoe","sup3rpaZZ");
-        jdoe = new User("0b6ae56859971bf3a34056e08b293b02", 1, "M5(ITYK");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ");
+        User jdoe = new User("0b6ae56859971bf3a34056e08b293b02", 1, "M5(ITYK");
         assertEquals(0, validator.isAuthenticationTest(jdoe, userInput));
     }
 
     @Test
     public void testResAndRole() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a");
-        res = new Resource();
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a");
+        Resource res = new Resource();
         assertEquals(0, validator.isAuthorizationTest(res, userInput));
     }
 
     @Test
     public void testChildRes() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b");
-        res = new Resource();
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b");
+        Resource res = new Resource();
         assertEquals(0, validator.isAuthorizationTest(res, userInput));
     }
 
     @Test
     public void testNotRole() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","XXX","a.b");
-        res = new Resource();
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","XXX","a.b");
+        Resource res = new Resource();
         assertEquals(3, validator.isAuthorizationTest(res, userInput));
     }
 
     @Test
     public void testNotRes() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","XXX");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","XXX");
         assertEquals(4, validator.isAuthorizationTest(null, userInput));
     }
 
     @Test
     public void testResWhitBadRole() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","WRITE","a");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","WRITE","a");
         assertEquals(4, validator.isAuthorizationTest(null, userInput));
     }
 
     @Test
     public void testNotResInDB() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","WRITE","a.bc");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","WRITE","a.bc");
         assertEquals(4, validator.isAuthorizationTest(null, userInput));
     }
 
     @Test
     public void testFullArg() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "100");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "100");
         assertEquals(0, validator.isAccoutingTest(userInput));
     }
 
     @Test
     public void testNotDe() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "01-01-2015", "2015-12-31", "100");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "01-01-2015", "2015-12-31", "100");
         assertEquals(5, validator.isAccoutingTest(userInput));
     }
 
     @Test
     public void testStringVol() {
-        userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "XXX");
+        UserInput userInput = new UserInput("jdoe","sup3rpaZZ","READ","a.b", "2015-01-01", "2015-12-31", "XXX");
         assertEquals(5, validator.isAccoutingTest(userInput));
     }
 
     @Test
     public void testAllocation() {
-        userInput = new UserInput();
+        UserInput userInput = new UserInput();
         String[] args = ("-login jdoe -pass sup3rpaZZ -role READ -res a.b -ds 2015-01-01 -de 2015-12-31 -vol 100").split(" ");
         assertEquals("jdoe", validator.allocation(userInput, args).getLogin());
         assertEquals("sup3rpaZZ", validator.allocation(userInput, args).getPass());
